0417387ba9be4f1a9be76a2aaaebe284d70e2e98
Uliana <124802136+UlianaTheBesd@users.noreply.github.com>
2025-09-20 18:54:03 +0300

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Новый тип "BankAccount" в котором будут поля и методы,
+ * необходимые для работы со счетом.
+ */
+
+public class BankAccount {
+    private static int counter = 1;
+    private int id;
+    private String name;
+    private double balance;
+    private List<String> history = new ArrayList<>();
+
+    public BankAccount(String n) {
+        this.id = counter++;
+        this.name = n;
+        this.balance = 0;
+        history.add("Создан аккаунт номер " + id + ".");
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public List<String> getHistory() {
+        return history;
+    }
+
+    public void add_amount(double amount) {
+        balance += amount;
+        history.add("Добавлено на счёт №"+ getId() + ": " + amount + ".");
+    }
+
+    public void substract_amount(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            history.add("Снято со счёта №"+ getId() + ": " + amount + ".");
+        }
+        else {
+            System.out.println("Недостаточно средств для снятия!");
+            history.add("Попытка снять со счёта №" + getId() + " сумму " + amount + "." + " Недостаточно средств.");
+        }
+    }
+
+    public void show_balance() {
+        System.out.println("Баланс счёта №"+ getId() + ": " + getBalance() + ".");
+        history.add("Показан баланс счёта номер " + getId() + ".");
+    }
+
+    public void print_history() {
+        for (String i : getHistory()) {
+            System.out.print(i + " "); // как end=" " в Python.
+        }
+        System.out.println();
+        history.add("Показана история счёта номер " + getId() + ".");
+    }
+}
